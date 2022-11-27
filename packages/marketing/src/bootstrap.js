4355676e6d5a23b7518985bc0f171e4ba620a6f9
Carloto <viniciuscarloto@gmail.com>
2022-11-27 12:03:17 -0300

    add history sync between container and marketing apps

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,17 +1,34 @@
+import { createMemoryHistory, createBrowserHistory } from 'history';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-const mount = (el) => {
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
   ReactDOM.render(
-    <App />,
+    <App history={history} />,
     el
-  )
+  );
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    }
+  }
 };
 
 if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
-  if (devRoot) mount(devRoot);
+  if (devRoot) mount(devRoot, { defaultHistory: createBrowserHistory() });
 }
 
 export { mount };
